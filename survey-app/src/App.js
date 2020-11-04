fae9e69e4ee79ebc8ece8ac42d2715dc6ba6934e
Kalpit <kalpits21@gmail.com>
2020-11-04 21:10:37 +0530

    changes to display component based on API's response

--- survey-app/src/App.js
+++ survey-app/src/App.js
@@ -1,9 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
-
+import Form from "./components/Form";
 function App() {
-  return <Login />;
+  return (
+    <div>
+      {localStorage.getItem("success") === "true" ? <Form /> : <Login />}
+    </div>
+  );
 }
 
 export default App;
