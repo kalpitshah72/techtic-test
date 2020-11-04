646f436f099ec8c7bd08160bcd8b95127f4a6a84
Kalpit <kalpits21@gmail.com>
2020-11-04 20:49:18 +0530

    added axios and request made to API

--- survey-app/src/components/Login.js
+++ survey-app/src/components/Login.js
@@ -2,25 +2,32 @@ import React from "react";
 import { Button } from "antd";
 import { GoogleLogin } from "react-google-login";
 import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import axios from "axios";
 
 export default function Login() {
   const responseGoogle = (data) => {
     if (data.tokenId !== undefined) {
-      let params = {
-        provider: "google",
-        token: data.tokenId,
-      };
-      console.log(params);
+      axios
+        .post(`http://localhost:5000/api/auth/google`, {
+          tokenId: data.tokenId,
+        })
+        .then((res) => {
+          console.log(res.data);
+          localStorage.setItem("success", res.data.success);
+        });
     }
   };
 
   const responseFacebook = (data) => {
     if (data.accessToken !== undefined) {
-      let params = {
-        provider: "facebook",
-        token: data.accessToken,
-      };
-      console.log(params);
+      axios
+        .post(`http://localhost:5000/api/auth/facebook`, {
+          token: data.accessToken,
+        })
+        .then((res) => {
+          console.log(res.data);
+          localStorage.setItem("success", res.data.success);
+        });
     }
   };
   return (
